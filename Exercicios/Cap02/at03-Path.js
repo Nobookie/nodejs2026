afa1e66e802a86a73471dd601d5be2033a87df3b
LEONARDO IGNACIO FERNANDES <leonardo.ifernandes@senacsp.edu.br>
2026-04-18 08:12:31 -0300

    Cap02 exercicios

--- Exercicios/Cap02/at03-Path.js
+++
@@ -1,7 +0,0 @@
-const path =require('path')
-let arquivo='./at03-Path.js'
-
-console.log('Extensão: ', path.extname(arquivo))
-console.log('nome completo: ', path.basename(arquivo))
-console.log('Unidade Base: ', path.dirname(arquivo))
-console.log('Caminho Absoluto: ', path.resolve(arquivo)) 
